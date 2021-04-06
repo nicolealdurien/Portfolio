5a4eb3b0942d89ede6fd82b9d66489df9ac3b5c9
naldurien <nicole.aldurien@gmail.com>
2021-04-06 17:13:58 -0400

    version deployed on surge, php contact form not working

--- php/process-form.php
+++ php/process-form.php
@@ -1,19 +1,14 @@
 <?php
 if (isset($_REQUEST['name'],$_REQUEST['email'])) {
-      
-    $name = $_REQUEST['name'];
-    $email = $_REQUEST['email'];
-    $message = $_REQUEST['message'];
-      
-    // Set your email address where you want to receive emails. 
-    $to = 'nicole.aldurien@gmail.com';
-      
-    $subject = 'Contact Request From Website';
-    $headers = "From: ".$name." <".$email."> \r\n";
-      
-    $send_email = mail($to,$subject,$message,$headers);
-      
-    echo ($send_email) ? 'success' : 'error';
-      
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];
+// Set your email address where you want to receive emails. 
+$to = 'nicole.aldurien@gmail.com';
+$subject = 'Contact Request From Website';
+$headers = "From: ".$name." <".$email."> \r\n";
+$send_email = mail($to,$subject,$message,$headers);
+echo ($send_email) ? 'success' : 'error';
 }
-?>
+?> 
+
